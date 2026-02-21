3bf735b391182a455561cbbfe61627f4d741d463
Rich-P-Martin <richard.martin@gov.bc.ca>
2026-02-20 21:49:15 -0800

    first draft

---
+++ 01_ingest_data.R
@@ -0,0 +1,12 @@
+#' go to https://www150.statcan.gc.ca/t1/tbl1/en/tv.action?pid=9810040401
+#' click add remove data
+#' geography==canada
+#' age==total
+#' highest certificate==select all items
+#' major field== all 4 digit
+#' noc == 5 digit
+#' gender==total
+#' download selected for database loading, move to add_to_pond
+
+library(bcgovpond)
+ingest_pond()
